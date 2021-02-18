8621df5302f050f5e6b886df46f53e71076dd8b2
Felipe Chernicharo <string7dev@gmail.com>
2021-02-18 13:20:06 -0300

    variants basics

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,15 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const baseVariants = {
+  hidden: { opacity: 0, x: "-100vw" },
+  visible: { opacity: 1, x: 0, transition: { type: "spring", delay: 0.5 } },
+};
+
+const nextVariants = {
+  startState: { x: "-100vw" },
+  endState: { x: 0, transition: { type: "spring", stiffness: 300 } },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <motion.div
       className="base container"
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", delay: 0.5 }}
+      variants={baseVariants}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -21,7 +31,6 @@ const Base = ({ addBase, pizza }) => {
               key={base}
               onClick={() => addBase(base)}
               whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
-              transition={{ type: "spring", stiffness: 300 }}
             >
               <span className={spanClass}>{base}</span>
             </motion.li>
@@ -32,9 +41,9 @@ const Base = ({ addBase, pizza }) => {
       {pizza.base && (
         <motion.div
           className="next"
-          initial={{ x: "-100vh", opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ type: "spring", stiffness: 120 }}
+          variants={nextVariants}
+          // initial="startState" -> child inherits animation execution from parent, so no need to declare initial / animate
+          // animate="endState"
         >
           <Link to="/toppings">
             <motion.button
