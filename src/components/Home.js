96ce7ef9ccadb1b0fa65510fd791f3df7abfdf8e
Felipe Chernicharo <string7dev@gmail.com>
2021-02-18 11:53:52 -0300

    initial ->  animate (start X end)

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,14 @@ import { motion } from "framer-motion";
 const Home = () => {
   return (
     <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       className="home container"
-      animate={{ rotateZ: [30, -30, 0], opacity: 0.4 }}
     >
-      <motion.h2 animate={{ fontSize: 50, color: "#ff2994", y: [50, -50, 0] }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+      <h2>Welcome to Pizza Joint</h2>
 
       <Link to="/base">
-        <motion.button animate={{ scale: 1.3 }}>
-          Create Your Pizza
-        </motion.button>
+        <button>Create Your Pizza</button>
       </Link>
     </motion.div>
   );
