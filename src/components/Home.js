a34d99bfb00129f7d7171bfedde1354a718fa7e9
Felipe Chernicharo <string7dev@gmail.com>
2021-02-18 11:38:30 -0300

    animate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      className="home container"
+      animate={{ rotateZ: [30, -30, 0], opacity: 0.4 }}
+    >
+      <motion.h2 animate={{ fontSize: 50, color: "#ff2994", y: [50, -50, 0] }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
+
       <Link to="/base">
-        <button>
+        <motion.button animate={{ scale: 1.3 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
